68b4a9023f8a1e28cb7a38df6a089dc6c24dcf2a
Fionn McCabe-Wild <fmccabewild@stu.naperville203.org>
2024-08-30 14:11:07 -0500

    reverse

--- Chapter 15 Activities/Reverse/src/ListUtil.java
+++ Chapter 15 Activities/Reverse/src/ListUtil.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * This class supplies a utility method to reverse the entries in a linked list.
@@ -12,6 +13,13 @@ public class ListUtil
     */
     public static void reverse(LinkedList<String> strings)
     {
-        ...
+        LinkedList<String> reversed = new LinkedList<>();
+        ListIterator<String> iterator = strings.listIterator();
+        while(iterator.hasNext()){
+            reversed.addFirst(iterator.next());
+            iterator.remove();
+        }
+        strings = reversed;
+        System.out.println(strings);
     }
 }
--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -9,6 +9,53 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
+        /*The addLast method can be used to populate a list. */
+        LinkedList<String> staff = new LinkedList<>();
+        staff.addLast("Tony");
+        staff.addLast("Natasha");
+        staff.addLast("Peter");
+        staff.addLast("Gamora");
+
+        System.out.println(staff);
+
+        ListIterator<String> iterator = staff.listIterator();
+        iterator.next();
+        String avenger = iterator.next();
+        System.out.println(avenger);
+        iterator.add("Steve");
+        iterator.add("Clint");
+        iterator.next();
+        iterator.remove();
+        iterator.previous();
+        iterator.set("T'Challa");
+
+        iterator = staff.listIterator();
+        while (iterator.hasNext()){
+            String n = iterator.next();
+            if (n.equals("Natasha")){
+                iterator.remove();
+            }
+        }
         
+        for (String n : staff){
+            System.out.print(n + " ");
+        }
+        System.out.println();
+
+        iterator = staff.listIterator();
+        while (iterator.hasNext()) {
+            String n = iterator.next();
+            if (n.equals("Tony")){
+                //staff.remove("Tony");
+            }
+        }
+
+        for (String n : staff)
+        {
+            if (n.equals("Tony"))
+            {
+                //staff.add("Bruce");
+            }
+        }
     }
 }
