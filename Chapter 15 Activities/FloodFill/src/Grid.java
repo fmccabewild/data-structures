f3ab08e1a73c24a3c8e22923a590f61f0b45ab84
Fionn McCabe-Wild <fmccabewild@stu.naperville203.org>
2024-09-23 14:14:13 -0500

    working

--- Chapter 15 Activities/FloodFill/src/Grid.java
+++ Chapter 15 Activities/FloodFill/src/Grid.java
@@ -1,15 +1,38 @@
+import java.util.Stack;
 public class Grid
 {
     private static final int SIZE = 10;
     int[][] pixels = new int[SIZE][SIZE];
-    . . .
+    int count = 1;
 
     /**
      * Flood fill, starting with the given row and column.
     */
     public void floodfill(int row, int column)
     {
-        . . .
+        Stack<Pair> floodfill = new Stack<>();
+        floodfill.push(new Pair(row, column));
+
+        while(!floodfill.isEmpty())
+        {
+            Pair current = floodfill.pop();
+            int cR = current.getRow();
+            int cC = current.getColumn();
+            if(within(current)& pixels[cR][cC] == 0)
+            {
+                pixels[cR][cC] = count;
+                count++;
+                floodfill.add(new Pair(cR-1, cC));
+                floodfill.add(new Pair(cR, cC));
+                floodfill.add(new Pair(cR-1, cC));
+                floodfill.add(new Pair(cR-1, cC));
+            }
+        }
+    }
+
+    public boolean within(Pair current)
+    {
+        return(current.getRow() >= 0 & current.getColumn() >= 0 & current.getRow() < 10 & current.getColumn() < 10);
     }
 
     @Override
